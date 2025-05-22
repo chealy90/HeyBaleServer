550dd1e3af02948d401450a93b06d577d540f31a
Christopher Healy <69329144+chealy90@users.noreply.github.com>
2025-05-22 21:02:18 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // firebase.js (for Node.js backend)
 const admin = require('firebase-admin');
 const path = require('path');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require(path.join(__dirname, 'serviceAccountKey.json'));
 
 
 
