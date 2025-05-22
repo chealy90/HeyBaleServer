f91c38f4250408b7fc4ac79289d85c7df8f560e9
Christopher Healy <69329144+chealy90@users.noreply.github.com>
2025-05-22 20:55:24 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 // firebase.js (for Node.js backend)
 const admin = require('firebase-admin');
 const path = require('path');
+const serviceAccount = require('./serviceAccountKey.json');
 
-// Load service account key
-const serviceAccount = require(path.resolve(__dirname, process.env.FIREBASE_PATH_TO_SERVICE_ACCOUNT_KEY));
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
