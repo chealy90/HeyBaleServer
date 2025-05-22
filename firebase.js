0364e3f2d19405e5f911b7af7e2df09d90e139be
Christopher Healy <69329144+chealy90@users.noreply.github.com>
2025-05-22 21:35:48 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // firebase.js (for Node.js backend)
 const admin = require('firebase-admin');
 const path = require('path');
-const serviceAccount = process.env.FIREBASE_SERVICE_KEY
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
 
 
 
