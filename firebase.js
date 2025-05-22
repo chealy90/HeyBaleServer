0033f02801d3a2cea0f9f1d188e592d90910fe67
chealy90 <christopherhealy20@gmail.com>
2025-05-22 21:29:19 +0100

    firebase fixes

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // firebase.js (for Node.js backend)
 const admin = require('firebase-admin');
 const path = require('path');
-const serviceAccount = require(path.join(__dirname, 'serviceAccountKey.json'));
+const serviceAccount = process.env.FIREBASE_SERVICE_KEY
 
 
 
