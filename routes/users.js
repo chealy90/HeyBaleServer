a1998fc459ea808b23e0071fd3b2d05917354eb6
chealy90 <christopherhealy20@gmail.com>
2025-06-03 01:31:49 +0100

    onboarding profile update route created

--- routes/users.js
+++ routes/users.js
@@ -19,19 +19,48 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    const userDocSnapShot = await usersModel.where('email', '==', req.body.email).limit(1).get()
-    if (!userDocSnapShot.empty){
-        const userDoc = userDocSnapShot.docs[0]
-        if (req.body.password === userDoc.password){
-            res.status(200).json(userDoc.data())
+    try {
+        const userDocSnapShot = await usersModel.where('email', '==', req.body.email).limit(1).get()
+        if (!userDocSnapShot.empty){
+            const userDoc = userDocSnapShot.docs[0]
+            if (req.body.password === userDoc.password){
+                res.status(200).json(userDoc.data())
+            }
+            else {
+                res.status(401).json({errorMessage: "Invalid email and password combination"})
+            }
         }
         else {
-            res.status(401).json({errorMessage: "Invalid email and password combination"})
+            res.status(500).json({errorMessage: "An error occurred while trying to log in:"})
+        }
+    } catch (error) {
+        res.status(500).json({errorMessage: error})
+    }
+    
+})
+
+router.post('/setUserInfo/:email', async (req, res) => {
+    try {
+        const userDocSnapShot = await usersModel.where('email', '==', req.params.email).limit(1).get()
+        //user not found
+        if (userDocSnapShot.empty){
+            res.status(404).json({errorMessage: "Error - could not find account with requested"})
+        }
+        else {
+            const userDoc = userDocSnapShot.docs[0]
+            const newUserObj = {
+                ...userDoc.data(),
+                ...req.body
+            }
+            await setDoc(userDoc.ref, newUserObj)
+            res.status(200).json({data: "Success"})
+
         }
     }
-    else {
-        res.status(500).json({errorMessage: "An error occurred while trying to log in"})
+    catch (error){
+        res.status(500).json({errorMessage: error})
     }
+    
 })
 
 
